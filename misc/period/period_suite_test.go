390c35ceb7b207ed8375e52d2e38b73c7d881320
outo <swalak@gmail.com>
2017-11-18 14:51:53 +0000

    ignore pprof output and remove pprof lines

--- misc/period/period_suite_test.go
+++ misc/period/period_suite_test.go
@@ -3,13 +3,10 @@ package period_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	//"github.com/pkg/profile"
 	"testing"
 )
 
 func TestPeriod(t *testing.T) {
-	//defer profile.Start(profile.CPUProfile).Stop()
-
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Period Suite")
 }
